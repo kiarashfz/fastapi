26da608f007df4b00e7d7c7e4b6ef47de7fe098c
Kiarash Fazli <myrad.fazli@gmail.com>
2022-08-02 10:30:14 +0430

    get_db add to database.py & password "@" injection handled

--- app/database.py
+++ app/database.py
@@ -2,12 +2,13 @@ from sqlalchemy import create_engine
 from sqlalchemy.ext.declarative import declarative_base
 from sqlalchemy.orm import sessionmaker
 from app.config import settings
+from urllib.parse import quote_plus as urlquote
 
 user = settings.pg_user
 password = settings.pg_password
 server = settings.pg_host
-db = settings.pg_database
-SQLALCHEMY_DATABASE_URL = f"postgresql://{user}:{password}@{server}/{db}"
+database = settings.pg_database
+SQLALCHEMY_DATABASE_URL = f"postgresql://{user}:%s@{server}/{database}" % urlquote(password)
 
 engine = create_engine(
     SQLALCHEMY_DATABASE_URL
@@ -15,3 +16,11 @@ engine = create_engine(
 SessionLocal = sessionmaker(autocommit=False, autoflush=False, bind=engine)
 
 Base = declarative_base()
+
+
+def get_db():
+    db = SessionLocal()
+    try:
+        yield db
+    finally:
+        db.close()
