e8e19de60420d763be5d5aa6626e38d5f3aa0118
Kiarash Fazli <myrad.fazli@gmail.com>
2022-08-03 21:03:16 +0430

    apis moved to routers module

---
+++ app/routers/posts.py
@@ -0,0 +1,131 @@
+from typing import List
+
+from fastapi import HTTPException, status, Depends, Response, APIRouter
+from sqlalchemy.orm import Session
+
+from app import models, schemas
+from app.database import get_db
+# from app.main import conn, cur
+
+router = APIRouter(
+    prefix="/posts"
+)
+
+
+# @router.get("/posts/latest")
+# async def latest_post():
+#     query = "SELECT * FROM posts ORDER BY created_at DESC LIMIT 1"
+#     cur.execute(query)
+#     last_post = cur.fetchone()
+#     return {"latest_post": last_post}
+#
+#
+# @router.get("/posts/{post_id}")
+# async def get_post(post_id: int, res: Response):
+#     # try:
+#     query = "SELECT * FROM posts WHERE id = %s"
+#     cur.execute(query, (post_id,))
+#     requested_post = cur.fetchone()
+#     if not requested_post:
+#         raise HTTPException(status_code=status.HTTP_404_NOT_FOUND, detail=f'post with id {post_id} doesn\'t exist!')
+#     return requested_post
+#     # except Exception as error:
+#     #     print(error)
+#     #     raise HTTPException(status_code=status.HTTP_500_INTERNAL_SERVER_ERROR, detail=str(error))
+#
+#
+# @router.get("/posts")
+# async def get_all_posts():
+#     query = "SELECT * FROM posts"
+#     cur.execute(query)
+#     all_posts = cur.fetchall()
+#     return {"posts": all_posts}
+#
+#
+# @router.post("/posts", status_code=status.HTTP_201_CREATED)
+# async def create_post(new_post: schemas.PostCreate):
+#     try:
+#         query = "INSERT INTO posts(title, content) VALUES(%s, %s) RETURNING *;"
+#         cur.execute(query, (new_post.title, new_post.content))
+#         inserted_post = cur.fetchone()
+#         conn.commit()
+#         return {"new_post": inserted_post}
+#     except Exception as error:
+#         print(error)
+#         raise HTTPException(status_code=status.HTTP_500_INTERNAL_SERVER_ERROR, detail=str(error))
+#
+#
+# @router.put("/posts/{post_id}")
+# async def update_post(post_id: int, updated_post: schemas.PostUpdate):
+#     # try:
+#     query = "UPDATE posts SET title = %s, content = %s, published = %s WHERE id = %s RETURNING *"
+#     cur.execute(query, (updated_post.title, updated_post.content, updated_post.published, post_id))
+#     changed_post = cur.fetchone()
+#     if not changed_post:
+#         raise HTTPException(status_code=status.HTTP_404_NOT_FOUND, detail=f"post with id {post_id} not found!")
+#     conn.commit()
+#     return {"updated_post": changed_post}
+#     # except Exception as error:
+#     #     raise HTTPException(status_code=status.HTTP_500_INTERNAL_SERVER_ERROR, detail=str(error))
+#
+#
+# @router.delete("/posts/{post_id}", status_code=status.HTTP_204_NO_CONTENT)
+# async def delete_post(post_id: int):
+#     # try:
+#     query = "DELETE FROM posts WHERE id = %s RETURNING id;"
+#     cur.execute(query, (post_id,))
+#     deleted_post = cur.fetchone()
+#     if not deleted_post:
+#         raise HTTPException(status_code=status.HTTP_404_NOT_FOUND, detail=f"post with id {post_id} not found!")
+#     conn.commit()
+#     return Response(status_code=status.HTTP_204_NO_CONTENT)
+#     # except Exception as error:
+#     #     print(error)
+#     #     raise HTTPException(status_code=status.HTTP_500_INTERNAL_SERVER_ERROR, detail=str(error))
+
+
+@router.get("/", response_model=List[schemas.PostResponse])
+async def all_posts_sqlalchemy(db: Session = Depends(get_db)):
+    posts = db.query(models.Post).all()
+    return posts
+
+
+@router.get("/{post_id}", response_model=schemas.PostResponse)
+async def specific_post_sqlalchemy(post_id, db: Session = Depends(get_db)):
+    post = db.query(models.Post).get(post_id)
+    # post = db.query(models.Post).filter(models.Post.id == post_id).first()
+    if not post:
+        raise HTTPException(status_code=status.HTTP_404_NOT_FOUND, detail=f"post with id {post_id} not found!")
+    return post
+
+
+@router.post("/", status_code=status.HTTP_201_CREATED, response_model=schemas.PostResponse)
+async def create_post_sqlalchemy(new_post: schemas.PostCreate, db: Session = Depends(get_db)):
+    try:
+        # created_post = models.Post(title=new_post.title, content=new_post.content, published=new_post.published)
+        created_post = models.Post(**new_post.dict())
+        db.add(created_post)
+        db.commit()
+        db.refresh(created_post)
+        return created_post
+    except Exception as error:
+        raise HTTPException(status_code=status.HTTP_500_INTERNAL_SERVER_ERROR, detail=str(error))
+
+
+@router.delete("/{post_id}", status_code=status.HTTP_204_NO_CONTENT)
+async def delete_post_sqlalchemy(post_id: int, db: Session = Depends(get_db)):
+    post = db.query(models.Post).get(post_id)
+    if not post:
+        raise HTTPException(status_code=status.HTTP_404_NOT_FOUND, detail=f"post with id {post_id} not found!")
+    db.delete(post)
+    db.commit()
+
+
+@router.put("/{post_id}", response_model=schemas.PostResponse)
+async def update_post_sqlalchemy(post: schemas.PostUpdate, post_id: int, db: Session = Depends(get_db)):
+    post = db.query(models.Post).filter(models.Post.id == post_id).update(post.dict())
+    if not post:
+        raise HTTPException(status_code=status.HTTP_404_NOT_FOUND, detail=f"post with id {post_id} not found!")
+    db.commit()
+    updated_post = db.query(models.Post).get(post_id)
+    return updated_post
---
+++ app/routers/users.py
@@ -0,0 +1,42 @@
+from typing import List
+
+import sqlalchemy.exc
+from fastapi import HTTPException, status, Depends, APIRouter
+from sqlalchemy.orm import Session
+
+from app import models, schemas
+from app.database import get_db
+from app.utils import password_hasher
+
+router = APIRouter(
+    prefix="/users"
+)
+
+
+@router.get("/", response_model=List[schemas.UserResponse])
+async def get_all_users(db: Session = Depends(get_db)):
+    all_users = db.query(models.User).all()
+    return all_users
+
+
+@router.post("/", status_code=status.HTTP_201_CREATED, response_model=schemas.UserResponse)
+async def create_user(new_user: schemas.UserCreate, db: Session = Depends(get_db)):
+    try:
+        hashed_password = password_hasher(new_user.password)
+        new_user.password = hashed_password
+        created_user = models.User(**new_user.dict())
+        db.add(created_user)
+        db.commit()
+        db.refresh(created_user)
+        return created_user
+    except sqlalchemy.exc.IntegrityError:
+        raise HTTPException(status_code=status.HTTP_409_CONFLICT,
+                            detail=f"User with email: {new_user.email} already exists!")
+
+
+@router.get("/{user_id}", response_model=schemas.UserResponse)
+async def get_user(user_id: int, db: Session = Depends(get_db)):
+    user = db.query(models.User).get(user_id)
+    if not user:
+        raise HTTPException(status_code=status.HTTP_404_NOT_FOUND, detail=f"User with id {user_id} not found!")
+    return user
