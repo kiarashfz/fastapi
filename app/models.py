7658c1cdd2bdb709fc4138175e03ef78c73cd90f
Kiarash Fazli <myrad.fazli@gmail.com>
2022-08-03 18:00:00 +0430

    User model created

--- app/models.py
+++ app/models.py
@@ -16,6 +16,15 @@ class Post(Base):
     created_at = Column(TIMESTAMP(timezone=True), server_default=text("NOW()"))
 
 
+class User(Base):
+    __tablename__ = "users"
+
+    id = Column(Integer, primary_key=True, index=True, nullable=False)
+    email = Column(String, unique=True, nullable=False)
+    password = Column(String, nullable=False)
+    created_at = Column(TIMESTAMP(timezone=True), server_default=text("NOW()"))
+
+
 # class User(Base):
 #     __tablename__ = "users"
 #
