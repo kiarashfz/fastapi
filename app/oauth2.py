a6b43daf2b66958935dfc3fb1aa9315b6e7e7762
Kiarash Fazli <myrad.fazli@gmail.com>
2022-08-11 17:56:31 +0430

    get_current_user separated from get_current_user_id and nea function don't hit the db

--- app/oauth2.py
+++ app/oauth2.py
@@ -43,29 +43,45 @@ def create_access_token(data: dict):
 def verify_access_token(access_token: str, auth_exception):
     try:
         payload = jwt.decode(access_token, SECRET_KEY, algorithms=ALGORITHM)
-        email: str = payload.get("email")
-        if not email:
+        user_id: str = payload.get("user_id")
+        if not user_id:
             raise auth_exception
-        token_data = schemas.TokenData(email=email)
+        token_data = schemas.TokenDataUserId(user_id=user_id)
         return token_data
     except JWTError as error:
         print(error)
         raise auth_exception
 
 
-def get_user(email: str, db: Session = Depends(get_db)):
+def get_user(user_id: int, db: Session = Depends(get_db)):
+    user = db.query(models.User).filter(models.User.id == user_id).first()
+    if user:
+        return user
+
+
+def get_user_by_email(email: str, db: Session = Depends(get_db)):
     user = db.query(models.User).filter(models.User.email == email).first()
     if user:
         return user
 
 
 def authenticate_user(email: str, password: str, db: Session = Depends(get_db)):
-    user = get_user(email, db)
+    user = get_user_by_email(email, db)
     if not user:
         return False
     if not verify_password(password, user.password):
         return False
     return user
+
+
+async def get_current_user_id(token: str = Depends(oauth2_scheme)):
+    credentials_exception = HTTPException(
+        status_code=status.HTTP_401_UNAUTHORIZED,
+        detail="Could not validate credentials",
+        headers={"WWW-Authenticate": "Bearer"},
+    )
+    token_data = verify_access_token(token, credentials_exception)
+    return token_data.user_id
 
 
 async def get_current_user(token: str = Depends(oauth2_scheme), db: Session = Depends(get_db)):
@@ -75,7 +91,7 @@ async def get_current_user(token: str = Depends(oauth2_scheme), db: Session = De
         headers={"WWW-Authenticate": "Bearer"},
     )
     token_data = verify_access_token(token, credentials_exception)
-    user = get_user(token_data.email, db)
+    user = get_user(token_data.user_id, db)
     if user is None:
         raise credentials_exception
     return user
