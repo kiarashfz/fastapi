fa69027d1c61f764377a07c96c44673c07774f9a
Kiarash Fazli <myrad.fazli@gmail.com>
2022-08-11 10:32:22 +0430

    query & variable definitions cleaned

--- app/graphqls.py
+++ app/graphqls.py
@@ -3,17 +3,14 @@ from strawberry.asgi import GraphQL
 
 
 @strawberry.input
-class UserId:
+class UserInp:
     user_id: int
     FName2: str
 
 
 @strawberry.type
 class User:
-    def __int__(self, user_id):
-        self.user_id = user_id
-
-    user_id: int
+    id: int
     name: str
     age: int
     fname: str
@@ -22,8 +19,8 @@ class User:
 @strawberry.type
 class Query:
     @strawberry.field
-    def user(self, user_id: UserId) -> User:
-        return User(user_id=user_id.user_id, name="Patrick", age=100, fname=user_id.FName2)
+    def user(self, user_obj: UserInp) -> User:
+        return User(id=user_obj.user_id, name="Patrick", age=100, fname=user_obj.FName2)
 
 
 schema = strawberry.Schema(query=Query)
@@ -31,17 +28,17 @@ schema = strawberry.Schema(query=Query)
 graphql_app = GraphQL(schema)
 
 ## query
-# query ($userId: UserId!){
-#     user(userId: $userId){
+# query ($variable_name: UserInp!){
+#     user(userObj: $variable_name){
 #         name
-#         userId
+#         id
 #         fname
 #     }
 # }
 
 ## define variable
 # {
-#     "userId": {
+#     "variable_name": {
 #         "userId": 6,
 #         "FName2": "hhh"
 #     }
