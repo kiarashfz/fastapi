0ea4adaf0e9df19edbbfbb827287bc776ea1da5a
Kiarash Fazli <myrad.fazli@gmail.com>
2022-08-17 17:47:10 +0430

    default values set fot settings & .env path changed

--- app/config.py
+++ app/config.py
@@ -2,19 +2,19 @@ from pydantic import BaseSettings
 
 
 class Settings(BaseSettings):
-    pg_host: str
-    pg_database: str
-    pg_user: str
+    pg_host: str = "localhost"
+    pg_database: str = "fastapi"
+    pg_user: str = "postgres"
     pg_password: str
     api_key: str
     secret_key: str
     algorithm: str
     access_token_expire_minutes: int
     api_key: str
-    api_key_name: str
+    api_key_name: str = "API_KEY"
 
     class Config:
-        env_file = "app/.env"
+        env_file = ".env"
 
 
 settings = Settings()
