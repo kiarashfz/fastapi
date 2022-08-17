1f5b5d6c78f98fffcc6e4806dc181d61ccefe0bb
Kiarash Fazli <myrad.fazli@gmail.com>
2022-08-17 17:12:43 +0430

    unrelated codes moved to related files

--- app/main.py
+++ app/main.py
@@ -1,12 +1,6 @@
-from time import sleep
-
-import psycopg2
-from fastapi import FastAPI, status, HTTPException, Depends
-from fastapi.security import OAuth2PasswordBearer
-from psycopg2.extras import RealDictCursor
+from fastapi import FastAPI, Depends
 
 from app import models, websockets
-from app.config import settings
 from app.database import engine
 from app.graphqls import graphql_app
 from app.oauth2 import get_api_key
@@ -18,44 +12,12 @@ app = FastAPI(
     dependencies=[Depends(get_api_key)]
 )
 
-app.add_route("/graphql", graphql_app)
-app.add_websocket_route("/graphql", graphql_app)
 app.include_router(posts.router)
 app.include_router(users.router)
 app.include_router(auth.router)
+app.add_route("/graphql", graphql_app)
+app.add_websocket_route("/graphql", graphql_app)
 app.include_router(websockets.router)
-
-# oauth2_scheme = OAuth2PasswordBearer(tokenUrl="token")
-# api_keys = [
-#     settings.api_key
-# ]  # This is encrypted in the database
-
-#
-# def api_key_auth(api_key: str = Depends(oauth2_scheme)):
-#     if api_key not in api_keys:
-#         raise HTTPException(
-#             status_code=status.HTTP_401_UNAUTHORIZED,
-#             detail="Forbidden"
-#         )
-
-
-def connect_to_db(host: str, database: str, user: str, password: str):
-    pg_connection = psycopg2.connect(host=host, database=database, user=user, password=password,
-                                     cursor_factory=RealDictCursor)
-    # Open a cursor to perform database operations
-    pg_cursor = pg_connection.cursor()
-    print('Database connection was successful')
-    return pg_connection, pg_cursor
-
-
-while True:
-    try:
-        conn, cur = connect_to_db(settings.pg_host, settings.pg_database, settings.pg_user, settings.pg_password)
-        break
-    except Exception as err:
-        print('Connecting to database failed!')
-        print(f'Error: {err}')
-        sleep(2)
 
 
 @app.get("/")
